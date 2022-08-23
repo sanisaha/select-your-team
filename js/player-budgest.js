0db486f00052c625a4d5f23959080e7da7607a5a
sanisaha <44751151+sanisaha@users.noreply.github.com>
2022-08-23 16:49:40 +0300

    all calculations are passed now

--- js/player-budgest.js
+++ js/player-budgest.js
@@ -7,3 +7,15 @@ document.getElementById("select-players").addEventListener("click", function () 
     totalPlayerCost.innerText = playerCost * totalSelectedFootballers;
 
 })
+
+document.getElementById("total-calculate").addEventListener("click", function () {
+    const totalFootballerCost = document.getElementById("total-player-cost");
+    const totalFootballerCostValue = parseInt(totalFootballerCost.innerText);
+    const managerCost = document.getElementById("manager-price");
+    const managerCostValue = parseInt(managerCost.value);
+    const coachCost = document.getElementById("coach-price");
+    const coachCostValue = parseInt(coachCost.value);
+    const totalExpenses = document.getElementById("total-expenses");
+    totalExpenses.innerText = totalFootballerCostValue + managerCostValue + coachCostValue;
+
+})
