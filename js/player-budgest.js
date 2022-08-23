6b5f722c07ae1e349239c580dd7168259449b84e
sanisaha <44751151+sanisaha@users.noreply.github.com>
2022-08-23 16:29:07 +0300

    player calculation js done

--- js/player-budgest.js
+++ js/player-budgest.js
@@ -2,10 +2,8 @@ document.getElementById("select-players").addEventListener("click", function () 
     const onePlayerCost = document.getElementById("player-price");
     const playerCost = parseInt(onePlayerCost.value);
     const totalPlayerCost = document.getElementById("total-player-cost");
-    let totalPlayerCostAmount = totalPlayerCost.innerText;
-    totalPlayerCostAmount = playerCost * 5;
-
-    // const plyerCostString = toString(playerCost);
-    console.log(totalPlayerCostAmount);
+    const SelectedFootballers = document.getElementById("player-number");
+    const totalSelectedFootballers = parseInt(SelectedFootballers.innerText);
+    totalPlayerCost.innerText = playerCost * totalSelectedFootballers;
 
 })
